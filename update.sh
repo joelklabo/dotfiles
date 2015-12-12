7edd6c7962637fafeb1f1ba88ddcac8357c12a25
Joel Klabo <joelklabo@gmail.com>
2015-12-12 17:12:39 +0000

    cleaning up dotfiles

--- update.sh
+++ update.sh
@@ -5,9 +5,5 @@
 
 cp -rf .vim ~/.vim
 cp -rf .vimrc ~/.vimrc
-cp -rf .gitconfig ~/.gitconfig
-cp -rf .gitignore ~/.gitignore
-cp -rf .oh-my-zsh/ ~/.oh-my-zsh
-cp -rf .zshrc ~/.zshrc
 
 echo "dotfiles have been copied"
